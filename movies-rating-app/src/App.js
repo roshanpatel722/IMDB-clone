0011bf7414098f87d548c47f691403c96033dc54
roshan <roshanpatel722@gmail.com>
2022-03-15 00:21:06 +0530

    movie app

---
+++ movies-rating-app/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import NavBar from "./components/NavBar";
+import Banner from './components/Banner';
+import Movies from './components/Movies'
+import Pagination from './components/Pagination'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Favourites from './components/Favourites'
+
+function App() {
+  return (
+    <BrowserRouter>
+
+      <NavBar></NavBar>
+      <Routes>
+        <Route path="/" element={<>
+          <Banner/>
+          <Movies />
+          <Pagination />
+        </>}/>
+        <Route path="/favourites" element={<Favourites />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
